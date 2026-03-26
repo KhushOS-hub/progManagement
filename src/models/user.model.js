486c226a71e59f7c51f1b7cd80ff5b6f20780f9d
KhushOS-hub <dev.kushagra06@gmail.com>
2026-03-26 12:40:42 +0530

    Defined access and refresh tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt"
-import { useReducer } from "react";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -70,6 +69,26 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+            _id: this._id,
+            email: this.email,
+            uesername: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    )   
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+)
 }
 
 export const user = mongoose.model("User", userSchema)
